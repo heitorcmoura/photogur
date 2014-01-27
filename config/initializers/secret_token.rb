20050253f2f85f675d21ca666ac724076f9b029b
“hcm90” <heitor.moura@gmail.com>
2014-01-27 13:42:01 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photogur::Application.config.secret_key_base = 'ea6fbde12f2c9e6c78f9057719d72a2e2338a11c87190aecdb830c3c1358573aae3f53bd3021485f07e9bd2310e9f64fb4a6b82b5309f56b83d0bc3450e54464'
