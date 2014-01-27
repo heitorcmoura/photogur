e2b12ed94189a496537f33845a839e63f93a36a3
“hcm90” <heitor.moura@gmail.com>
2014-01-27 16:02:31 -0500

    Simplified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,6 @@
 Photogur::Application.routes.draw do
+  resources :pictures
   root :to => "pictures#index"
-  get 'pictures' => 'pictures#index'
-
-  post 'pictures' => "pictures#create" # this is a new line of code
-  get 'pictures/new' => 'pictures#new' # this is also a new line of code
-
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-  patch 'pictures/:id' => "pictures#update"
-
-  delete 'pictures/:id' => 'pictures#destroy', as: "picture"
-
-  root :to => "pictures#index"
-
-  get 'pictures/:id' => 'pictures#show', as: "picture"
 
 
 
